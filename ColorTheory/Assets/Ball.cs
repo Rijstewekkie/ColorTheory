e0a987d728d46e53cd4bebf90398580ed0256a0a
Mika van Suilichem <mikavansuilichem09@gmail.com>
2025-09-10 11:38:51 +0200

    poosh

---
+++ ColorTheory/Assets/Ball.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("FallingBlock"))
+        {
+            collision.gameObject.GetComponent<FallingBlock>().Falling = true;
+        }
+    }
+}
