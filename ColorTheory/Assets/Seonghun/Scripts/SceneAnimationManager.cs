d252cf5781ddfadb61dd13591ba5d607b7263cc4
Mabrium7 <gangseonghun24@gmail.com>
2025-09-16 11:56:02 +0200

    tutorial update, change script name, make anim

--- ColorTheory/Assets/Seonghun/Scripts/SceneAnimationManager.cs
+++ ColorTheory/Assets/Seonghun/Scripts/SceneAnimationManager.cs
@@ -5,6 +5,10 @@ public class SceneAnimationManager : MonoBehaviour
     [SerializeField] private Next[] nextScripts;
     [SerializeField] int i = 0;
 
+    private void Update()
+    {
+        NextCode();
+    }
 
     public interface OnNext
     {
