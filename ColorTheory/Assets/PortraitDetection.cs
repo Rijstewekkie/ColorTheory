cb4854dd266fbe278c8a078cc02230f93880fce2
Mika van Suilichem <mikavansuilichem09@gmail.com>
2025-09-17 15:47:17 +0200

    anti portrait stuff

---
+++ ColorTheory/Assets/PortraitDetection.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PortraitDetection : MonoBehaviour
+{
+    public GameObject AntiPortraitMessage;
+    void Update()
+    {
+        if (Screen.height > Screen.width) // Portrait
+        {
+            if (!AntiPortraitMessage.activeSelf)
+                AntiPortraitMessage.SetActive(true);
+        }
+        else // Landscape
+        {
+            if (AntiPortraitMessage.activeSelf)
+                AntiPortraitMessage.SetActive(false);
+        }
+    }
+}
